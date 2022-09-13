97ff43cc6727ac29b7a85e0d7fbe2b192b3a58ad
fujitasyuro <syuro.dev@gmail.com>
2022-09-13 16:10:14 +0700

    xay dung chi tiet san pham

---
+++ project/lib/ui/products/products_manager.dart
@@ -0,0 +1,17 @@
+import '../../models/product.dart';
+
+class ProductsManager {
+  final List<Product> _items = [];
+
+  int get itemCount {
+    return _items.length;
+  }
+
+  List<Product> get items {
+    return [..._items];
+  }
+
+  List<Product> get favoriteItems {
+    return _items.where((prodItem) => prodItem.isFavorite).toList();
+  }
+}
