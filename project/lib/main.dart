e3f403f41d96e3f5d30b3cbfbb35a3dd6f8ffa1a
fujitasyuro <syuro.dev@gmail.com>
2022-09-14 19:46:33 +0700

    Xay dung trang cac san pham nguoi dung

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'ui/products/products_manager.dart';
 import 'ui/products/product_detail_screen.dart';
 import 'ui/products/product_overview_screen.dart';
+import 'ui/products/user_products_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,8 @@ class MyApp extends StatelessWidget {
             )),
         home: SafeArea(
           // child: ProductDetailScreen(ProductsManager().items[0]),
-          child: ProductOverviewScreen(),
+          // child: ProductOverviewScreen(),
+          child: UserProductsScreen(),
         ));
   }
 }
