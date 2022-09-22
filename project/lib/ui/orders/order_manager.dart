5e2f629e0e9736501a8691a48b447cff80806206
Syuro <syuro.dev@gmail.com>
2022-09-22 13:11:14 +0700

    update order_item_card.dart

---
+++ project/lib/ui/orders/order_manager.dart
@@ -0,0 +1,28 @@
+import '../../models/cart_item.dart';
+import '../../models/order_item.dart';
+
+class OrderManager with ChangeNotifier {
+  final List<OrderItem> _orders = [
+    OrderItem(
+      id: 'o1',
+      amount: 59.98,
+      products: [
+        CartItem(
+          id: 'c1',
+          title: 'Red Shirt',
+          price: 29.99,
+          quantity: 2,
+        )
+      ],
+      dateTime: DateTime.now(),
+    )
+  ];
+
+  int get orderCount {
+    return _orders.length;
+  }
+
+  List<OrderItem> get orders {
+    return [..._orders];
+  }
+}
